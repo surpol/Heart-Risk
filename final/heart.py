e8abee27b44f4f7c39264e808cf11f75d4515d2f
surpol <suryapolina@gmail.com>
2020-05-02 23:38:44 -0400

    classifiers implemented

--- final/heart.py
+++ final/heart.py
@@ -3,12 +3,18 @@
 """
 Created on Sat May  2 17:18:14 2020
 
-@author: suryapolina ramkammari
+@authors: suryapolina and ramkammari
 """
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.linear_model import Perceptron
+from sklearn.naive_bayes import GaussianNB
+from sklearn.naive_bayes import BernoulliNB
+from sklearn.ensemble import RandomForestClassifier
+from imblearn.over_sampling import SMOTE
 
 train_file = open("train.csv")
 train_file = list(train_file)
@@ -40,5 +46,22 @@ df = pd.DataFrame(train_dic.items(),columns=['Factors', 'Label'])
 
 X = df['Factors']
 y = df['Label']
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.33)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.35)
 
+#KNN
+knn = KNeighborsClassifier(n_neighbors = 100)
+knn.fit(list(X_train), y_train)
+prediction = knn.predict(list(X_test))
+score_knn = knn.score(list(X_test), y_test)
+
+#decision tree
+dt = DecisionTreeClassifier(criterion="gini", splitter='best')
+dt.fit(list(X_train), y_train)
+prediction_decision = dt.predict(list(X_test))
+score_decision = dt.score(list(X_test), y_test)
+
+#random forest
+rf = RandomForestClassifier(max_depth = 200)
+rf.fit(list(X_train), y_train)
+prediction_random_forest = rf.predict(list(X_test))
+score_random_forest = rf.score(list(X_test), y_test)
